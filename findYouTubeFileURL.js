8628f46a832edd3769420d208c0b68d8493b199e
Don Johnson <don.johnson@wonkspace.com>
2014-09-14 17:47:34 -0400

    First commit

---
+++ findYouTubeFileURL.js
@@ -0,0 +1,10 @@
+exports.findYouTubeFileURL = function (inURL,callback){
+
+	// retrieve arguments as array
+	var args = [];
+	for (var i = 0; i < arguments.length; i++) {
+		args.push(arguments[i]);
+	}
+	console.log("inURL: ",inURL);
+	return "dummy";
+}
